ce9e9792b38c9a66761b4f11f2d2cd5ed29ff393
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-05-05 14:01:52 -0400

    Add #add_child and #remove_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -10,13 +10,25 @@ class PolyTreeNode
   def parent=(other_node)
     previous_parent = @parent
     @parent = other_node
-    
+
     unless previous_parent.nil?
       previous_parent.children.delete(self)
     end
 
     return if other_node.nil?
     other_node.children << self unless other_node.children.include?(self)
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    if children.include?(child_node)
+      child_node.parent = nil
+    else
+      raise "that's not your child!"
+    end
   end
 
 end
