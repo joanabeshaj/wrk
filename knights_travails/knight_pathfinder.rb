db4343a357993a0286829eb94357bd44da91cbe3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-05-05 17:33:24 -0400

    Add #build_move_tree; partially implemented

--- knights_travails/knight_pathfinder.rb
+++ knights_travails/knight_pathfinder.rb
@@ -1,10 +1,12 @@
 require_relative 'tree_node'
+require 'byebug'
 
 class KnightPathFinder
-  attr_reader :start_position, :visited_positions
+  attr_reader :start_position, :visited_positions, :move_tree
 
   def self.valid_moves(pos)
     # self.new(pos)
+    #debugger
     x, y = pos
     moves = [
       [x - 1, y + 2],
@@ -24,13 +26,19 @@ class KnightPathFinder
 
   def initialize(start_position)
     @start_position = start_position
-    @move_tree = self.build_move_tree
+    @move_tree = []
     @visited_positions = [start_position]
-    # @possible_moves = KnightPathFinder.valid_moves(start_position)
   end
 
   def build_move_tree
+    queue = [start_position]
+    until queue.empty?
+      first_node = queue.shift
+      move_tree << PolyTreeNode.new(first_node)
+      queue.concat(new_move_positions(first_node))
+    end
 
+    puts "tree constructed!"
   end
 
   def new_move_positions(pos)
@@ -43,6 +51,6 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   k = KnightPathFinder.new([0,0])
-  k.new_move_positions([0,0])
-  p k.visited_positions
+  k.build_move_tree
+  p k.move_tree
 end
